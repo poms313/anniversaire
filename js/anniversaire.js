0acafe9e61a0d4bbd9992fe763f43b58b6732cb8
poms313 <pommine@free.fr>
2018-10-28 22:06:39 +0100

    click sur le troisieme paragraphe apparition du cadeau

--- js/anniversaire.js
+++ js/anniversaire.js
@@ -14,3 +14,40 @@ $un.on("click", (function () {
 $deux.on("click", (function () {
 	$trois.fadeIn(800);
 }));
+
+// Le troisième modifie la page
+$trois.on("click", (function () {
+
+	//Disparition des éléments présents
+	$('audio#son')[0].play();
+	$('#paragraphes').slideUp();
+	$('#titre').slideUp();
+
+	//Lancement de l'effet sonore
+	$('audio#son')[0].play();
+
+	//Modification de l'arriére plan
+	$('body').css("backgroundImage", "url(../son_images/cadeau.jpg)");
+	$('body').css("backgroundSize", "10%");
+
+	//Ajout de l'image	
+	var $cadeau = new Image();
+	$cadeau.src = "../son_images/voiture.png";
+	$cadeau.alt = "Voici ton cadeau"
+	$cadeau.width = 900;
+
+
+	// Du texte
+	$versionFinale.append("<h2 class=\"clignoter titre\">Voici ton cadeau</h2>");
+	$versionFinale.append($cadeau);
+	$versionFinale.append("<h2 class=\"clignoter titre\">C'est ton jour de chance!</h2>");
+
+	// Et d'animations
+	$versionFinale.slideDown(2000, function () {
+		function FaireClignoter() {
+			$('.clignoter').fadeIn(800).delay(1000).fadeOut(800);
+		}
+		var intervalId = setInterval(FaireClignoter, 700);
+	});
+
+}))
